84243f0cd7c001055ede3c202849a95b7f2e7e75
PaperCups <102964724+PaperCupsRay@users.noreply.github.com>
2024-12-26 15:09:33 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -51,6 +51,22 @@ export default {
 	async fetch(request, env, ctx) {
 		try {
 			async function getConfig(url) {
+	const Base64 = {
+		encode(str) {
+		    // 首先，我们使用 encodeURIComponent 来获得百分比编码的UTF-8，然后我们将百分比编码转换为原始字节，最后存储到btoa里面
+		    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+			function toSolidBytes(match, p1) {
+			    return String.fromCharCode(Number('0x' + p1));
+			}));
+		},
+		decode(str) {
+		    // 过程：从字节流到百分比编码，再到原始字符串
+		    return decodeURIComponent(atob(str).split('').map(function (c) {
+			return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+		    }).join(''));
+		}
+	}
+
 	let newapi = null;
 	try {
 		const response = await fetch(url, {
@@ -62,7 +78,8 @@ export default {
 		let jsondata = await response.json();
 		//gitee内容
 		if (url.startsWith("https://gitee.com/api/v5/repos/")) {
-			newapi = JSON.parse(atob(jsondata.content));
+    
+			newapi = JSON.parse(Base64.decode(jsondata.content));
 		} else {
 			newapi = jsondata;
 		}
